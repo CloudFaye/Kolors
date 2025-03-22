25cc5f2ead7857cd06b0c8cc5b1ea64e4ce17860
Faye seun <samuelfaye96@gmail.com>
2025-03-22 16:31:33 +0100

    small fixes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -47,7 +47,7 @@
     <h2 id="feedback-heading">Suggestions and Feedback</h2>
     <p>
       Kolors is a WIP 🚧. If you have suggestions, feedback, or would like to see a specific brand or design system featured,
-      <a href="https://mailto:Samuelfaye96@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email Faye Seun">Email me here</a> or send a message on X <a href="https://x.com/fayeseun_" target="_blank" rel="noopener noreferrer" aria-label="Contact Faye Seun on X (formerly Twitter)">here</a>.
+      <a href="mailto:Samuelfaye96@gmail.com?subject=Kolors%20Feedback&body=Hello%20Faye%2C%0A%0AI%20have%20some%20feedback%20about%20Kolors%3A%0A%0A" target="_blank" class='underline' rel="noopener noreferrer" aria-label="Email Faye Seun">Email me here</a> or send a message on X <a href="https://x.com/fayeseun_" target="_blank" rel="noopener noreferrer" aria-label="Contact Faye Seun on X (formerly Twitter)">here</a>.
     </p>
   </div>
 </div>
